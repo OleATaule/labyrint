71b1623d355f71c72f90f7298491e4a84b672c14
OleATaule <oleandreat@uia.no>
2024-12-13 16:50:29 +0100

    attempt2

---
+++ dictionary.mjs
@@ -0,0 +1,37 @@
+const DICTIONARY = {
+
+    en : {
+    lootGained: "Player gained ",
+    foundItem: "Player found a ",
+    attributeChanged: " is changed by ",
+    playerAttack: "Player dealt ",
+    pointsOfDmg: " points of damage",
+    bastardDied: " and the bastard died",
+    bastardDeals: "Bastard deals ",
+    back: " back",
+    itemSword: "Sword",
+    itemSpear: "Spear",
+    itemDagger: "Dagger",
+    foundDoor: "Found a door"
+},
+
+no : {
+    lootGained: "Spilleren mottok ",
+    foundItem: "Spilleren fant et ",
+    attributeChanged: " endres med ",
+    playerAttack: "Spilleren deala ",
+    pointsOfDmg: " poeng med skade",
+    bastardDied: " og fienden døde.",
+    bastardDeals: "Fienden deler ",
+    back: " tilbake",
+    itemSword: "Sverd",
+    itemSpear: "Spyd",
+    itemDagger: "Dagger",
+    foundDoor: "Fant ei dør"
+
+}
+
+}
+
+
+export default DICTIONARY;
